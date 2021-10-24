2723a6aa7d096369ed51f131bf889ffc56f4db9b
MishaKravchenko <mishakravchenko.1998@gmail.com>
2021-10-24 20:39:26 +0300

    homeWork

---
+++ Lesson3/main.js
@@ -0,0 +1,217 @@
+// - Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому.
+// 1) Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// a) отримує текст з параграфа з id "content"
+let content = document.getElementById("content");
+console.log(content.textContent);
+
+// b) отримує текст з блоку з id "rules"
+let rules = document.getElementById("rules");
+console.log(rules.textContent);
+
+// c) замініть текст параграфа з id 'content' на будь-який інший
+content.textContent = ("Signed by the Dodgers as a free agent in 1984, Hernández became the second Venezuelan catcher in MLB history, following Bo Díaz. He made his major league debut in the 1990 season, basically as a backup for Mike Scioscia. A high skilled defensive player, Hernández spent his Dodgers career as mainly a back-up catcher to Scioscia and Mike Piazza, appearing in 230 games, including 133 starts. He signed as a free agent with San Diego and later was traded to the Cardinals.");
+console.log(content.textContent);
+
+// d) замініть текст параграфа з id 'rules' на будь-який інший
+rules.textContent = ("Subreddit to help you find streams for every NBA game in one place with community provided streams. watch, free, basketball, online, regular, finals, regular, season, summer, league, championship, nba draft, nba schedule, nbastreams reddit, reddit nba streams, reddit nba live, nba scores, live nba, espn live, nba 2020 - 2021. Watch official NHL games streams in HD @60fps for free with the NHLGames application on Windows. Every game provides at least 3 different TV networks in French and in English. You can stream a live game, rewind it, replay it, or even download it to a file.")
+
+// e) змініть кожному елементу колір фону на червоний
+// f) змініть кожному елементу колір тексту на синій
+content.style.backgroundColor = ("red");
+content.style.color = ("blue");
+
+rules.style.backgroundColor = ("red");
+rules.style.color = ("blue");
+
+// g) отримати весь список класів елемента з id=rules і вивести їх в console.log
+let rulesClass = document.getElementsByClassName('fc bp')
+console.log(rulesClass);
+
+// h) отримати всі елементи з класом fc_rules
+
+let fc_rules = document.getElementsByClassName("fc_rules");
+console.log(fc_rules)
+
+// i) поміняти колір тексту у всіх елементів fc_rules на червоний
+for (let i = 0; i < fc_rules.length; i++) {
+    fc_rules[i].style.color = ("red");
+    fc_rules[i].style.backgroundColor = ("blue");
+}
+
+////________________________________________________________________________________________________
+let users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: { city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1 }
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: { city: 'New York', country: 'USA', street: 'East str', houseNumber: 21 }
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: { city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78 }
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: { city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56 }
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: { city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39 }
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: { city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5 }
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: { city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90 }
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: { city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33 }
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: { city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1 }
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: { city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4 }
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: { city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45 }
+}];
+
+// 2) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//     Вставити цей блок на сторінку
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i]);
+    let userDiv = document.createElement("div");
+    userDiv.textContent = (`${users[i].name} - -  ${users[i].age}, ${users[i].status}`);
+    document.body.appendChild(userDiv);
+}
+
+// 3) Блок з адресою зробити окремим блоком, з блоками для кожної властивості
+
+for (let i = 0; i < users.length; i++) {
+    let userDivAdress = document.createElement("div");
+
+    userDivAdress.textContent = (`
+    ${users[i].address.city},
+    ${users[i].address.country},
+    ${users[i].address.street},
+    ${users[i].address.houseNumber}`);
+
+    document.body.appendChild(userDivAdress);
+}
+
+// ______________________________________________________________________________________________________________________________________________________
+// РОБОТА В АУДИТОРІЇ
+// ______________________________________________________________________________________________________________________________________________________
+//
+// Взяти файл template_2.html та працювати в ньому
+// 1) Напишіть код, який :
+// a) змінює колір тексту елемнту з ід main_header на назву групи (mon-year)
+// b) робить шириниу елементу ul 400px
+// c) робить шириниу всіх елементів з класом linkList шириною 50%
+// d) отримує текст який зберігається в елементі з класом listElement2
+// e) отримує всі елементи li та змінює ім колір фону на сірий
+// f) отримує всі елементи 'a' та додає їм клас anchor
+// g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+// h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+// i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
+// j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
+// k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
+// l) отримати елементи p та змінити їм padding на 20px
+// m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year)
+//
+// 2) Є масив котрий характеризує правила.
+//     Створити скрипт який ітерує цей масив, та робить кожне правило в окремому блоці.
+//     При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+//     Результатом відпрацювання скріпта повинна бути така ж структура яка міститься в файлі rules.html
+//
+// let rules = [
+//     {
+//         title: 'Первое правило Бойцовского клуба.',
+//         body: 'Никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Второе правило Бойцовского клуба.',
+//         body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Третье правило Бойцовского клуба.',
+//         body: 'В схватке участвуют только двое.'
+//     },
+//     {
+//         title: 'Четвертое правило Бойцовского клуба.',
+//         body: 'Не более одного поединка за один раз.'
+//     },
+//     {
+//         title: 'Пятое правило Бойцовского клуба.',
+//         body: 'Бойцы сражаются без обуви и голые по пояс.'
+//     },
+//     {
+//         title: 'Шестое правило Бойцовского клуба.',
+//         body: 'Поединок продолжается столько, сколько потребуется.'
+//     },
+//     {
+//         title: 'Седьмое правило Бойцовского клуба.',
+//         body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+//     },
+//     {
+//         title: 'Восьмое и последнее правило Бойцовского клуба.',
+//         body: 'Новичок обязан принять бой.'
+//     }
+// ];
+//
+// ===========================================================================
+//
+// - Дано 2 масиви з рівною кількістю об'єктів.
+// Масиви:
+//     let usersWithId = [
+//         {id: 1, name: 'vasya', age: 31, status: false},
+//         {id: 2, name: 'petya', age: 30, status: true},
+//         {id: 3, name: 'kolya', age: 29, status: true},
+//         {id: 4, name: 'olya', age: 28, status: false}
+//     ];
+//
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+//
+// З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+//     Записати цей об'єкт в новий масив
+// Example:
+//     let usersWithCities = [
+//         {
+//             id: 1, // <===
+//             name: 'vasya',
+//             age: 31,
+//             status: false,
+//             address: {
+//                 user_id: 1, // <===
+//                 country: 'Ukraine',
+//                 city: 'Ternopil'
+//             }
+//         },
+//         // TO BE CONTINUED .....
+//     ]

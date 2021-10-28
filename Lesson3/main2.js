d3b500443031be6646795d7d3d226769b3931955
MishaKravchenko <mishakravchenko.1998@gmail.com>
2021-10-28 12:35:19 +0300

    classWork

--- Lesson3/main2.js
+++ Lesson3/main2.js
@@ -139,35 +139,38 @@ let rules = [
 ];
 
 for (let i=0;i<rules.length;i++){
-
+let divRules = document.createElement('div');
+divRules.textContent=(`${rules[i].title}${rules[i].body}`);
+document.body.appendChild(divRules);
+    console.log(divRules[i]);
 }
 
 
-
-
-
-
-
-
+let divElement = document.getElementsByTagName("div");
+for (let i=0;i<divElement.length;i++){
+    divElement[i].classList.add("rules");
+}
+console.log(divElement);
+ ///////////////////////////сложно
 
 
 // ===========================================================================
 //
 // - Дано 2 масиви з рівною кількістю об'єктів.
 // Масиви:
-//     let usersWithId = [
-//         {id: 1, name: 'vasya', age: 31, status: false},
-//         {id: 2, name: 'petya', age: 30, status: true},
-//         {id: 3, name: 'kolya', age: 29, status: true},
-//         {id: 4, name: 'olya', age: 28, status: false}
-//     ];
-//
-// let citiesWithId = [
-//     {user_id: 3, country: 'USA', city: 'Portland'},
-//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-//     {user_id: 2, country: 'Poland', city: 'Krakow'},
-//     {user_id: 4, country: 'USA', city: 'Miami'}
-// ];
+    let usersWithId = [
+        {id: 1, name: 'vasya', age: 31, status: false},
+        {id: 2, name: 'petya', age: 30, status: true},
+        {id: 3, name: 'kolya', age: 29, status: true},
+        {id: 4, name: 'olya', age: 28, status: false}
+    ];
+
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'}
+];
 //
 // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
 //     Записати цей об'єкт в новий масив
@@ -183,6 +186,14 @@ for (let i=0;i<rules.length;i++){
 //                 country: 'Ukraine',
 //                 city: 'Ternopil'
 //             }
-//         },
+//         }
 //         // TO BE CONTINUED .....
-//
+let usersWithCities = usersWithId.slice()
+for (let user of usersWithCities) {
+    for (let city of citiesWithId) {
+        if (user.id === city.user_id) {
+            user.address = city
+        }
+    }
+}
+console.log(usersWithCities);

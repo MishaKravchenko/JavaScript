c7a41c7a798f3bbd91d2d6a60b34deb83e13bed2
MishaKravchenko <mishakravchenko.1998@gmail.com>
2021-12-12 19:26:37 +0200

    CallBackHell.Promise.AsyncAwait

--- Lesson9/main.js
+++ Lesson9/main.js
@@ -487,6 +487,126 @@
 // })()
 
 
+// Реалізувати друкарську машинку.
+// У вас є текст "Hello World".
+// Ваша функція має друкувати цей текст по 1 симоволу в браузері.
+// КОЖНА нова буква має бути з РАНДОМНОЮ заутримкою від 0.1 до 1 секунди.
+// Цим самим ви маєте симулювати написання даного тексту юзером.
+// Приклад: "Hello"
+// Затримки:
+// H (затримка 0.6)
+// e (затримка 0.1)
+// l (затримка 0.3)
+// l (затримка 0.7)
+// о (затримка 1)
+
+// let text = 'Hello_World';
+// let div = document.createElement('div');
+// document.body.appendChild(div);
+// let random = Math.random()*1000;
+//
+// function print(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText = add[0]);
+//         }, random);
+//     });
+// }
+//
+// function print1(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[1]);
+//         }, random);
+//     });
+// }
+//
+// function print2(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[2]);
+//         }, random);
+//     });
+// }
+//
+// function print3(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[3]);
+//         }, random);
+//     });
+// }
+//
+// function print4(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[4]);
+//         }, random);
+//     });
+// }
+//
+// function print5(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[5]);
+//         }, random);
+//     });
+// }
+//
+// function print6(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[6]);
+//         }, random);
+//     });
+// }
+//
+// function print7(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[7]);
+//         }, random);
+//     });
+// }
+//
+// function print8(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[8]);
+//         }, random);
+//     });
+// }
+//
+// function print9(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[9]);
+//         }, random);
+//     });
+// }
+//
+// function print10(add) {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve(div.innerText += add[10]);
+//         }, random);
+//     });
+// }
+//
+// async function word() {
+//     let H = await print(text);
+//     let e = await print1(text);
+//     let l = await print2(text);
+//     let ll = await print3(text);
+//     let o = await print4(text);
+//     let space = await print5(text);
+//     let W = await print6(text);
+//     let oo = await print7(text);
+//     let r = await print8(text);
+//     let lll = await print9(text);
+//     let d = await print10(text);
+// }
+// word();
 
 
 
